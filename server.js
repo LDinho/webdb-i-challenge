b519dc519c8440d459c79b34a4a4385c9bafc6eb
LD <LDinho@users.noreply.github.com>
2019-05-23 18:59:22 -0400

    add get endpoint to retrieve all accounts

--- server.js
+++ server.js
@@ -2,6 +2,41 @@ const express = require('express');
 
 const server = express();
 
-// your code here
+const db = require('./data/accounts-model');
+
+const {
+  find,
+  findById,
+  add,
+  remove,
+  update
+} = db;
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Database Playground</h2>`)
+});
+
+server.get('/accounts', async (req, res) => {
+
+  try {
+    const accounts = await find();
+
+    if (accounts.length) {
+      return res.status(200).json({accounts});
+    }
+
+    res.status(400)
+       .json({message: 'No accounts found'});
+  }
+  catch (err) {
+    res.status(500)
+       .json({
+         error: 'Server error',
+         err
+       })
+  }
+})
 
 module.exports = server;
